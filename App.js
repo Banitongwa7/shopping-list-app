e1fb7a445a78d9db27446d440840dc6efcc63c8b
David BANITONGWA <davidbanitongwa@gmail.com>
2023-11-22 19:35:40 +0100

    update App.js

--- App.js
+++ App.js
@@ -1,20 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, FlatList, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+
+  const [product, setProduct] = useState('');
+  const [myProducts, setMyProducts] = useState([]);
+
+  const handleChangeText = (text) => {
+    setProduct(text);
+  };
+
+  const handleSubmit = () => {
+    setMyProducts([...myProducts, product]);
+    setProduct('');
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.textInput}
+        placeholder="Entrez un produit"
+        onChangeText={handleChangeText}
+      />
+      <Button
+        title="Valider"
+        onPress={handleSubmit}
+      />
+      </View>
+
+      <ScrollView>
+        <FlatList
+          data={myProducts}
+          renderItem={({ item }) => <Text>{item}</Text>}
+          keyExtractor={item => item}
+        />
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+   padding: 40,
+   paddingTop: 60,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+  },
+  textInput: {
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 5,
+    paddingLeft: 9,
+    flexGrow: 1,
   },
 });
